9f0af71b104341f78905f866f609d2662cf3cb6c
Manuel Cerda <espaciomore@gmail.com>
2014-04-21 22:09:21 -0400

    design change about message and footer now more stylish

--- private/client/client.js
+++ private/client/client.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
+  // show text
   localization();
+
   $('.localization').on('click',function(e){
     var el = $(this);
     var trans = el.data('lang');
     var lang = $('html').attr('lang');
-    // set new language
+    
     el.attr('href','#lang-'+trans);
     el.data('lang',lang);
     el.text(lang.toUpperCase());
     $('html').attr('lang',trans);
-    // replace translations
+    
     localization(trans);
   });
 });
